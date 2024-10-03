b7ee3ed40ae8a9a9472f53c220c880429866c13b
Marko Jovicic <markojovicic.rs@gmail.com>
2024-10-03 17:09:51 +0200

    Functional cart

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 from store.models import Product
 
@@ -28,11 +31,13 @@ def store(request, slug=None):
 def product_details(request, category_slug=None, product_slug=None):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'product': single_product,
+        'in_cart': in_cart
     }
 
     return render(request, 'store/product_details.html', context)
