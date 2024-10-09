be1da9de3d92cef4a2afd4bab626b4224ba9cce7
Marko Jovicic <markojovicic.rs@gmail.com>
2024-10-09 13:52:55 +0200

    Security Measures

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,10 +20,11 @@ from django.urls import path, include
 from . import views, settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('adminsl/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
