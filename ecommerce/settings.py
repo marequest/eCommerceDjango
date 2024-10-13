c5cb64141f04aa044a0a49ce8906198f4e1d1c99
Marko Jovicic <markojovicic.rs@gmail.com>
2024-10-13 22:14:49 +0200

    hmm

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -193,6 +193,9 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Storage backend settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 
 
 MESSAGE_TAGS = {
