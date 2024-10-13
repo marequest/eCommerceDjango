4922cb052167602b1ecfd8d8c60c81168b349b7c
Marko Jovicic <markojovicic.rs@gmail.com>
2024-10-13 18:15:41 +0200

    Fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -171,10 +171,10 @@ STATICFILES_DIRS = [
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # media files configuration
--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+from ecommerce import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.MEDIA_ROOT)
