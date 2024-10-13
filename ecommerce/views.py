09f3680122927540b150154ba709abce13452950
Marko Jovicic <markojovicic.rs@gmail.com>
2024-10-13 19:20:16 +0200

    S3 fix

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -6,10 +6,14 @@ from store.models import Product, ReviewRating
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_at')
+    reviews = []
 
     # Get reviews
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True).order_by('id')
+    if products is not None:
+        for product in products:
+            reviews = ReviewRating.objects.filter(product_id=product.id, status=True).order_by('id')
+    else:
+        products = []
 
     context = {
         'products': products,
